13df2961a6e4bacdf317db55f57e71ccd7780ec5
GinPC <laughlamz@gmail.com>
2021-03-13 18:47:26 +0700

    fetchReadme

--- src/containers/GithubDetail.js
+++ src/containers/GithubDetail.js
@@ -7,19 +7,18 @@ import ListRepo from "../components/ListRepo";
 export default function GithubDetail() {
     const { user, repo } = useParams();
     console.log('xxx', user, repo)
-    // const [readme, setReadme] = useState('');
+    const [readme, setReadme] = useState('');
 
-    // const fetchReadMe = useCallback(async () => {
-    //     const { status, data } = await axios.get(API.LIST_REPO(user));
-    //     console.log('xxx', data)
-    //     if (status === 200) {
-    //         setReadme(data);
-    //     }
-    // }, [user])
+    const fetchReadMe = useCallback(async () => {
+        const { status, data } = await axios.get(API.GET_README(user, repo));
+        if (status === 200) {
+            setReadme(data);
+        }
+    }, [repo, user])
 
-    // useEffect(() => {
-    //     fetchReadMe();
-    // }, [fetchReadMe]);
+    useEffect(() => {
+        fetchReadMe();
+    }, [fetchReadMe]);
 
     return (
         <div>
